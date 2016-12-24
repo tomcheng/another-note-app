cc4086ab7b1b225428f0035204a800809790b729
Thomas Cheng <thomascheng81@gmail.com>
2016-12-24 18:35:34 -0500

    Style checked items

---
+++ src/components/ListItem.js
@@ -0,0 +1,45 @@
+import React, { PropTypes, Component } from "react";
+
+class ListItem extends Component {
+  static propTypes = {
+    item: PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      value: PropTypes.string.isRequired,
+      checked: PropTypes.bool.isRequired,
+    }).isRequired,
+    listId: PropTypes.number.isRequired,
+    onUpdateListItem: PropTypes.func.isRequired,
+  };
+
+  handleChange = ({ target }) => {
+    const { onUpdateListItem, listId, item } = this.props;
+    onUpdateListItem({ listId, itemId: item.id, updates: { checked: target.checked } })
+  };
+
+  render () {
+    const { item } = this.props;
+    return (
+      <div style={{
+        height: 40,
+        display: "flex",
+        alignItems: "center",
+      }}>
+        <label>
+          <input
+            type="checkbox"
+            checked={item.checked}
+            onChange={this.handleChange}
+          />
+          <span style={{
+            textDecoration: item.checked ? "line-through" : null,
+            opacity: item.checked ? 0.3 : null,
+          }}>
+            {item.value}
+          </span>
+        </label>
+      </div>
+    );
+  }
+}
+
+export default ListItem;
