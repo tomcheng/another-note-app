62e5f70ddf1741802412f4498e5f08deaba4aca6
Thomas Cheng <thomascheng81@gmail.com>
2016-12-26 15:03:37 -0700

    Fix checked items going over popover

--- src/components/NoteMenu.js
+++ src/components/NoteMenu.js
@@ -101,6 +101,7 @@ class NoteMenu extends Component {
               right: 7,
               backgroundColor: "#fff",
               boxShadow: "0 3px 6px rgba(0,0,0,0.16), 0 3px 6px rgba(0,0,0,0.23)",
+              zIndex: 1,
             }}>
               {selectedNote.type !== "list" && (
                 <PopoverItem onClick={this.handleClickConvertToList}>
