8ade8dc0a4ff8ed93a07ea3d51b408adc98aba8d
Thomas Cheng <thomascheng81@gmail.com>
2016-12-26 00:19:30 -0700

    Move redux setup to index.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,8 @@
 import React, { Component } from 'react';
-import { createStore, applyMiddleware } from "redux";
-import { Provider } from "react-redux";
-import createSagaMiddleware from "redux-saga";
-import reducer from "./reducer";
-import sagas from "./sagas";
 import Search from "./components/Search";
 import Notes from "./components/Notes";
 import Preview from "./components/Preview";
 import './App.css';
-
-const sagaMiddleware = createSagaMiddleware();
-const middleware = [sagaMiddleware];
-
-if (process.env.NODE_ENV === "development") {
-  middleware.push(require("./utils/reduxLogger").default);
-}
-
-const store = createStore(reducer, applyMiddleware(...middleware));
-
-sagas.forEach(saga => { sagaMiddleware.run(saga); });
 
 class App extends Component {
   constructor (props) {
@@ -28,6 +12,7 @@ class App extends Component {
       appHeight: window.innerHeight,
     };
   }
+
   componentDidMount () {
     window.addEventListener("resize", () => {
       this.setState({ appHeight: window.innerHeight });
@@ -38,22 +23,20 @@ class App extends Component {
     const { appHeight } = this.state;
 
     return (
-      <Provider store={store}>
-        <div
-          style={{
-            display: "flex",
-            flexDirection: "column",
-            overflow: "hidden",
-            height: appHeight,
-          }}
-        >
-          <div style={{ flexShrink: 0 }}>
-            <Search />
-          </div>
-          <Notes containerStyle={{ flexShrink: 0, flexBasis: "30%", flexGrow: 1, overflow: "auto" }} />
-          <Preview containerStyle={{ flexShrink: 1, minHeight: "30%", }} />
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          overflow: "hidden",
+          height: appHeight,
+        }}
+      >
+        <div style={{ flexShrink: 0 }}>
+          <Search />
         </div>
-      </Provider>
+        <Notes containerStyle={{ flexShrink: 0 }} />
+        <Preview containerStyle={{ flexGrow: 1 }} />
+      </div>
     );
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,9 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore, applyMiddleware } from "redux";
+import { Provider } from "react-redux";
+import createSagaMiddleware from "redux-saga";
+import reducer from "./reducer";
+import sagas from "./sagas";
 import App from './App';
 import './index.css';
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
+const sagaMiddleware = createSagaMiddleware();
+const middleware = [sagaMiddleware];
+
+if (process.env.NODE_ENV === "development") {
+  middleware.push(require("./utils/reduxLogger").default);
+}
+
+const store = createStore(reducer, applyMiddleware(...middleware));
+
+sagas.forEach(saga => { sagaMiddleware.run(saga); });
+
+ReactDOM.render((
+  <Provider store={store}>
+    <App />
+  </Provider>
+), document.getElementById('root'));
