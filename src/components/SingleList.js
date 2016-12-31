bad2535c7f3b4554fa6ed6fdcf1a343ea0c382f4
Thomas Cheng <thomascheng81@gmail.com>
2016-12-31 02:24:13 -0500

    Use Card component in SingleList

--- src/components/SingleList.js
+++ src/components/SingleList.js
@@ -1,10 +1,16 @@
 import React, { PropTypes, Component } from "react";
+import { Link } from "react-router";
+import Card from "./Card";
 
 class SingleList extends Component {
   static propTypes = {
     list: PropTypes.shape({
       id: PropTypes.number.isRequired,
       title: PropTypes.string.isRequired,
+      items: PropTypes.arrayOf(PropTypes.shape({
+        id: PropTypes.number.isRequired,
+        value: PropTypes.string.isRequired,
+      })).isRequired,
     }).isRequired,
   };
 
@@ -12,7 +18,22 @@ class SingleList extends Component {
     const { list } = this.props;
 
     return (
-      <div>{list.title}</div>
+      <Card
+        header={(
+          <Link to="/" style={{ display: "block", padding: "10px 12px", fontWeight: 600 }}>
+            {list.title}
+          </Link>
+        )}
+        body={(
+          <div style={{ padding: "10px 12px 12px", minHeight: 66 }}>
+            {list.items.map(({ id, value }) => (
+              <div key={id}>
+                {value}
+              </div>
+            ))}
+          </div>
+        )}
+      />
     );
   }
 }
--- src/components/SingleNote.js
+++ src/components/SingleNote.js
@@ -22,10 +22,7 @@ class SingleNote extends Component {
           </Link>
         )}
         body={(
-          <pre style={{
-            padding: "10px 12px 12px",
-            minHeight: 66,
-          }}>
+          <pre style={{ padding: "10px 12px 12px", minHeight: 66 }}>
             {note.body}
           </pre>
         )}
