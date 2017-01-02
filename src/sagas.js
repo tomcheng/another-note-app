bdb950c232356c50951aff776950f721d2d58bf6
Thomas Cheng <thomascheng81@gmail.com>
2017-01-02 16:43:16 -0500

    Update sagas based on updated redux-saga package

--- src/sagas.js
+++ src/sagas.js
@@ -1,33 +1,38 @@
-import { takeEvery } from "redux-saga";
-import { put, call } from "redux-saga/effects";
+import { put, call, takeEvery } from "redux-saga/effects";
 import { actions } from "./reducer";
 import * as api from "./repo";
 
 const sagas = [];
 
 sagas.push(function* () {
-  yield* takeEvery("REQUEST_NOTES", function* () {
+  yield takeEvery("REQUEST_NOTES", function* () {
     const response = yield call(api.getNotes);
     yield put(actions.loadNotes(response));
   });
 });
 
 sagas.push(function* () {
-  yield* takeEvery("REQUEST_ADD_LIST", function* ({ payload }) {
-    const response = yield call(api.addList, payload);
-    yield put(actions.addNote(response));
-  });
-});
-
-sagas.push(function* () {
-  yield* takeEvery("REQUEST_ADD_NOTE", function* ({ payload }) {
+  yield takeEvery("REQUEST_ADD_NOTE", function* ({ payload }) {
     const response = yield call(api.addNote, payload);
     yield put(actions.addNote(response));
+    if (payload.callback) {
+      yield call(payload.callback, response);
+    }
   });
 });
 
 sagas.push(function* () {
-  yield* takeEvery("REQUEST_UPDATE_NOTE", function* ({ payload }) {
+  yield takeEvery("REQUEST_ADD_LIST", function* ({ payload }) {
+    const response = yield call(api.addList, payload);
+    yield put(actions.addNote(response));
+    if (payload.callback) {
+      yield call(payload.callback, response);
+    }
+  });
+});
+
+sagas.push(function* () {
+  yield takeEvery("REQUEST_UPDATE_NOTE", function* ({ payload }) {
     const response = yield call(api.updateNote, payload);
     yield put(actions.updateNote(response));
     if (payload.callback) {
@@ -37,7 +42,7 @@ sagas.push(function* () {
 });
 
 sagas.push(function* () {
-  yield* takeEvery("REQUEST_DELETE_NOTE", function* ({ payload }) {
+  yield takeEvery("REQUEST_DELETE_NOTE", function* ({ payload }) {
     yield call(api.deleteNote, payload);
     yield put(actions.deleteNote(payload));
     if (payload.callback) {
@@ -47,21 +52,21 @@ sagas.push(function* () {
 });
 
 sagas.push(function* () {
-  yield* takeEvery("REQUEST_CONVERT_NOTE_TO_LIST", function* ({ payload }) {
+  yield takeEvery("REQUEST_CONVERT_NOTE_TO_LIST", function* ({ payload }) {
     const response = yield call(api.convertToList, payload);
     yield put(actions.updateNote(response));
   });
 });
 
 sagas.push(function* () {
-  yield* takeEvery("REQUEST_ADD_LIST_ITEM", function* ({ payload }) {
+  yield takeEvery("REQUEST_ADD_LIST_ITEM", function* ({ payload }) {
     const response = yield call(api.addListItem, payload);
     yield put(actions.updateNote(response));
   });
 });
 
 sagas.push(function* () {
-  yield* takeEvery("REQUEST_UPDATE_LIST_ITEM", function* ({ payload }) {
+  yield takeEvery("REQUEST_UPDATE_LIST_ITEM", function* ({ payload }) {
     const response = yield call(api.updateListItem, payload);
     yield put(actions.updateNote(response));
   });
